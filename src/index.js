9efdbdbe3511f2e31e88cf792e9226baeee53765
Daniel Wiesner <daniel_wiesner@gmx.de>
2021-02-04 14:36:34 +0100

    Add card component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import Header from './components/Header'
 import Button from './components/Button'
+import Card from './components/Card'
 
-const header = Header()
+const header = Header('Quiz App', 'May the better quizzer win!')
 document.body.append(header)
 
 const button = Button('Click Me!')
 document.body.append(button)
+
+const card = Card(
+  'Which city is the capital of Germany',
+  "Berlin is Germany's capital"
+)
+document.body.append(card)
